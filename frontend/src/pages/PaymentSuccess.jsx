6cde41ab334bab3ae9cda8b6969866dac66ecce0
ashishcoderoofs <coderoofitsolutions@gmail.com>
2025-10-25 14:13:05 +0530

    Reapply "Merge branch 'main' of https://github.com/coderoofsit/Mixer-UI"
    This reverts commit d8ceedc33df361dda653c76829bdb928d7a81cac.

--- frontend/src/pages/PaymentSuccess.jsx
+++ frontend/src/pages/PaymentSuccess.jsx
@@ -1,21 +1,58 @@
-import React, { useEffect } from "react";
-import { useLocation, useParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
+import authService from "../services/authService";
+import { stripeService } from "../services/stripeService";
+const producTypeMap = {
+	background_check: "com.mixerltd.mixerltd.background_check",
+	quarterly_membership: "com.mixerltd.mixerltd.upgrade",
+};
 
 export default function PaymentSuccess() {
 	const location = useLocation();
+	const navigate = useNavigate();
+	const [userDetails, setUserDetails] = useState();
+	const params = new URLSearchParams(location.search);
+	const sessionId = params.get("session_id");
+	const productType = params.get("productType");
+	console.log({ sessionId, productType });
 	useEffect(() => {
-		const params = new URLSearchParams(location.search);
-		const sessionId = params.get("session_id");
-		console.log({ sessionId });
-	}, []);
-
-    const dummyFunction = () => {
-    //     if (sessionId) {
-    //   fetch(`http://localhost:4242/api/v1/stripe/status?session_id=${sessionId}`)
-    //     .then(res => res.json())
-    //     .then(data => console.log('Payment verified:', data))
-    //     .catch(console.error);
-    // }
-    }
-	return <div>Payment added succesfully</div>;
+		console.log({ sessionId, productType });
+		const getProfile = async () => {
+			const profileCheck = await authService.checkProfileCompletion();
+			console.log({ profileCheck });
+			const userDetails = profileCheck?.profile || {};
+			console.log({ userDetails });
+			setUserDetails(userDetails || {});
+		};
+		if (!userDetails) {
+			getProfile();
+		}
+		const dummyFunction = async () => {
+			const response = await stripeService.verifyPaymentInIAP(
+				userDetails?.id,
+				producTypeMap[productType],
+				sessionId,
+			);
+			console.log({ response });
+		};
+		if (productType && userDetails) {
+			dummyFunction();
+		}
+	}, [userDetails, userDetails]);
+	console.log({ userDetails });
+	return (
+		<div className='flex items-center justify-center min-h-screen bg-gray-100'>
+			<div className='bg-white p-8 rounded-xl shadow-lg text-center'>
+				<h2 className='text-2xl font-semibold text-green-600 mb-4'>
+					✅ Payment added successfully
+				</h2>
+				<button
+					onClick={() => navigate("/")}
+					className='mt-4 px-6 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition duration-300'
+				>
+					Go to Home
+				</button>
+			</div>
+		</div>
+	);
 }
